8e3c971a9abdfd6b772ff62242b9f9d5921eb8b0
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-13 19:29:16 +0100

    Initial Commit

---
+++ Bank/BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankApp.swift
+//  Bank
+//
+//  Created by  Ixart on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
